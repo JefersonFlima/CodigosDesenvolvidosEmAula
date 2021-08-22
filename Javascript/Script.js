e7e0e742aeb6fd1dd704545c8649a11209ebba31
Jeferson Fagundes de Lima <38568439+JefersonFlima@users.noreply.github.com>
2021-08-22 18:00:04 -0300

    Add files via upload

---
+++ Javascript/Script.js
@@ -0,0 +1,8 @@
+function Johny() {
+    document.getElementById('paragrafo').innerHTML = Date();
+}
+
+function mudar() {
+    document.getElementById("paragrafo").style.fontSize = "20px";
+    document.getElementById("paragrafo").style.color = "#f5f500";
+}
